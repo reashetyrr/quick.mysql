ab800864e49db4c141e399e4067a7d81fbce79f7
reashetyrr <6582364+reashetyrr@users.noreply.github.com>
2020-02-25 15:27:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -83,31 +83,31 @@ module.exports = {
         return arbitrate('set', {stringify: true, id: key, data: value, ops: ops || {}});
     },
 
-    /**
-     * This function adds a number to a key in the database. (If no existing number, it will add to 0)
-     * @param {key} input any string as a key. Also allows for dot notation following the key.
-     * @param {options} [input={ target: null }] Any options to be added to the request.
-     * @returns {data} the updated data.
-     */
-
-    add: function(key, value, ops) {
-        if (!key) throw new TypeError('No key specified');
-        if (isNaN(value)) throw new TypeError('Must specify value to add');
-        return arbitrate('add', {id: key, data: value, ops: ops || {}});
-    },
-
-    /**
-     * This function subtracts a number to a key in the database. (If no existing number, it will subtract from 0)
-     * @param {key} input any string as a key. Also allows for dot notation following the key.
-     * @param {options} [input={ target: null }] Any options to be added to the request.
-     * @returns {data} the updated data.
-     */
-
-    subtract: function(key, value, ops) {
-        if (!key) throw new TypeError('No key specified');
-        if (isNaN(value)) throw new TypeError('Must specify value to add');
-        return arbitrate('subtract', {id: key, data: value, ops: ops || {}});
-    },
+    // /**
+    //  * This function adds a number to a key in the database. (If no existing number, it will add to 0)
+    //  * @param {key} input any string as a key. Also allows for dot notation following the key.
+    //  * @param {options} [input={ target: null }] Any options to be added to the request.
+    //  * @returns {data} the updated data.
+    //  */
+    //
+    // add: function(key, value, ops) {
+    //     if (!key) throw new TypeError('No key specified');
+    //     if (isNaN(value)) throw new TypeError('Must specify value to add');
+    //     return arbitrate('add', {id: key, data: value, ops: ops || {}});
+    // },
+    //
+    // /**
+    //  * This function subtracts a number to a key in the database. (If no existing number, it will subtract from 0)
+    //  * @param {key} input any string as a key. Also allows for dot notation following the key.
+    //  * @param {options} [input={ target: null }] Any options to be added to the request.
+    //  * @returns {data} the updated data.
+    //  */
+    //
+    // subtract: function(key, value, ops) {
+    //     if (!key) throw new TypeError('No key specified');
+    //     if (isNaN(value)) throw new TypeError('Must specify value to add');
+    //     return arbitrate('subtract', {id: key, data: value, ops: ops || {}});
+    // },
 
     /**
      * This function will push into an array in the database based on the key. (If no existing array, it will create one)
@@ -123,61 +123,61 @@ module.exports = {
     },
 
 
-    /**
-
-     */
-
-    /**
-     * This function will delete an object (or property) in the database.
-     * @param {key} input any string as a key. Also allows for dot notation following the key, this will delete the prop in the object.
-     * @param {options} [input={ target: null }] Any options to be added to the request.
-     * @returns {boolean} if it was a success or not.
-     */
-
-    delete: function(key, ops) {
-        if (!key) throw new TypeError('No key specified');
-        return arbitrate('delete', {id: key, ops: ops || {}});
-    },
-
-    /**
-     * This function returns a boolean indicating whether an element with the specified key exists or not.
-     * @param {key} input any string as a key. Also allows for dot notation following the key, this will return if the prop exists or not.
-     * @param {options} [input={ target: null }] Any options to be added to the request.
-     * @returns {boolean} if it exists.
-     */
-
-    has: function(key, ops) {
-        if (!key) throw new TypeError('No key specified');
-        return arbitrate('has', {id: key,  ops: ops || {}});
-    },
-
-    includes: function(key, ops) {
-        if (!key) throw new TypeError('No key specified');
-        return arbitrate('has', {id: key,  ops: ops || {}});
-    },
-
-    /**
-     * This function fetches the entire active table
-     * @param {options} [input={ target: null }] Any options to be added to the request.
-     * @returns {boolean} if it exists.
-     */
-
-    all: function(ops) {
-        return arbitrate('all', {ops: ops || {}});
-    },
-
-    fetchAll: function(ops) {
-        return arbitrate('all', {ops: ops || {}});
-    },
-
-
-    /* 
-    * Used to get the type of the value.
-    */
-
-
-    type: function(key, ops) {
-        if (!key) throw new TypeError('No key specified');
-        return arbitrate('type', {id: key, ops: ops || {}});
-    },
+    // /**
+    //
+    //  */
+    //
+    // /**
+    //  * This function will delete an object (or property) in the database.
+    //  * @param {key} input any string as a key. Also allows for dot notation following the key, this will delete the prop in the object.
+    //  * @param {options} [input={ target: null }] Any options to be added to the request.
+    //  * @returns {boolean} if it was a success or not.
+    //  */
+    //
+    // delete: function(key, ops) {
+    //     if (!key) throw new TypeError('No key specified');
+    //     return arbitrate('delete', {id: key, ops: ops || {}});
+    // },
+    //
+    // /**
+    //  * This function returns a boolean indicating whether an element with the specified key exists or not.
+    //  * @param {key} input any string as a key. Also allows for dot notation following the key, this will return if the prop exists or not.
+    //  * @param {options} [input={ target: null }] Any options to be added to the request.
+    //  * @returns {boolean} if it exists.
+    //  */
+    //
+    // has: function(key, ops) {
+    //     if (!key) throw new TypeError('No key specified');
+    //     return arbitrate('has', {id: key,  ops: ops || {}});
+    // },
+    //
+    // includes: function(key, ops) {
+    //     if (!key) throw new TypeError('No key specified');
+    //     return arbitrate('has', {id: key,  ops: ops || {}});
+    // },
+    //
+    // /**
+    //  * This function fetches the entire active table
+    //  * @param {options} [input={ target: null }] Any options to be added to the request.
+    //  * @returns {boolean} if it exists.
+    //  */
+    //
+    // all: function(ops) {
+    //     return arbitrate('all', {ops: ops || {}});
+    // },
+    //
+    // fetchAll: function(ops) {
+    //     return arbitrate('all', {ops: ops || {}});
+    // },
+    //
+    //
+    // /*
+    // * Used to get the type of the value.
+    // */
+    //
+    //
+    // type: function(key, ops) {
+    //     if (!key) throw new TypeError('No key specified');
+    //     return arbitrate('type', {id: key, ops: ops || {}});
+    // },
 };
